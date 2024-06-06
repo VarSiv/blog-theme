d06394d53e2176c785f0f3da1f47f5e0886d2e94
Varya <varya@MacBook-Pro-2.local>
2024-06-06 05:04:54 +0300

    added images to correct folder, moved header and footer to their own files

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
+//dynamic title tag support
+add_theme_support( 'title_tag' );
+
 function blue_register_styles(){
     $version = wp_get_theme()->get('Version'); 
     wp_enqueue_style('blue-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
     wp_enqueue_style('blue-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
-    wp_enqueue_style('blue-custom', get_template_directory_uri()."/style.css", array('blue-boostrap'), $version, 'all');
+    wp_enqueue_style('blue-custom', get_template_directory_uri()."/style.css", array(), $version, 'all');
 }
 add_action('wp_enqueue_scripts', 'blue_register_styles');
 
