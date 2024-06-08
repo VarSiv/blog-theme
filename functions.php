3db2a849f3123feaa2fb916fc64098f392def158
Siv <var.mironov@gmail.com>
2024-06-08 06:09:04 +0300

    made post rendering dynamic, used metadata like post age/tags to make shit prettier and actually functional

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ function blue_theme_support(){
     //dynamic title tag support
     add_theme_support( 'title-tag' );
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 add_action( 'after_setup_theme', 'blue_theme_support' );
 
---
+++ template-parts/content-article.php
@@ -0,0 +1,16 @@
+<div class="container">
+	<header class="content-header">
+        <!--change the_date() params to get it in desired format, like age in months, day of the week pubblished, etc copilot linked this lol (https://developer.wordpress.org/reference/functions/the_date/)-->
+	    <div class="meta mb-3"><span class="date"><?php the_date();?></span>
+        <?php
+            the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>',);
+        ?>
+
+
+        <span class="comment"><a href="#comments"><i class='fa fa-comment'></i><?php comments_number()?></a></span></div>
+	</header>
+
+    <?php
+        the_content();
+    ?>
+</div>
